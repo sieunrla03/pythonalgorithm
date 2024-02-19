007380a71864829308c2d48db9009d8f8010b3cf
kse98 <kse9832@naver.com>
2024-02-19 22:48:11 +0900

    240219

--- 240131/11720.py
+++ 240131/11720.py
@@ -1,3 +1,11 @@
 # 백준 - 숫자의 합
 N = int(input())
 print(sum(map(int,input())))
+'''
+n = input()
+nums = input()
+total = 0
+for i in range(n) :  # 0부터 n-1까지
+    total += int(nums[i])
+print(total)
+'''
